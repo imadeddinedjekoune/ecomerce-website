b9d17bc92bb87050a8fc4b174f4a443c7c53c985
imadeddinedjekoune <imaddjekoune@gmail.com>
2023-09-10 10:29:08 -0700

    comm4

--- project/project/settings.py
+++ project/project/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ecomm-webapp2-5aecb966c6f5.herokuapp.com" , "127.0.0.1"]
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
